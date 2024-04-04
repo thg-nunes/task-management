e41429cac5f314538b92d1867e6c5e09fba79c29
thiago nunes <desenvolvedor.nunes@gmail.com>
2024-04-04 20:38:16 -0300

    feat: adiciona os campos de token e refresh_token no schema prisma

--- prisma/migrations/20240403181035_create_users_model/migration.sql
+++ prisma/migrations/20240403181035_create_users_model/migration.sql
@@ -3,6 +3,8 @@ CREATE TABLE "Users" (
     "id" TEXT NOT NULL,
     "username" TEXT NOT NULL,
     "email" TEXT NOT NULL,
+    "token" TEXT,
+    "refresh_token" TEXT,
     "password" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
