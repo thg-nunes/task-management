0b0dd8916b42853b261cb8ae6708fff3134d9e64
thiago nunes <desenvolvedor.nunes@gmail.com>
2024-04-04 16:28:17 -0300

    refactor: atribuindo tipagem ao retorno da createUser função e usando utils/bcrypt

--- src/graphql/schema/users/datasources.ts
+++ src/graphql/schema/users/datasources.ts
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt'
+import { passwordCompareHash, passwordHash } from '@utils/bcrypts'
 
 import { PostgresDataSource } from '@dataSources/postgres'
 import { CreateUserInput } from './types'
@@ -6,20 +6,18 @@ import { CreateUserInput } from './types'
 type CreateUserData = Pick<CreateUserInput, 'userData'>
 
 export interface UsersDataSourceServices {
-  createUser({ userData }: CreateUserData): Promise<any>
+  createUser({ userData }: CreateUserData): Promise<User>
 }
 
 export class UsersDataSource
   extends PostgresDataSource
   implements UsersDataSourceServices
 {
-  private BCRYPT_SALT = 10
-
   constructor() {
     super()
   }
 
-  async createUser({ userData }: CreateUserData): Promise<any> {
+  async createUser({ userData }: CreateUserData): Promise<User> {
     const usernameAlreadyExists = await this.db.users.findUnique({
       where: {
         username: userData.username,
@@ -30,10 +28,7 @@ export class UsersDataSource
       throw new Error(`Username "${userData.username}" já existe, use outro.`)
     }
 
-    const encryptedPassword = await bcrypt.hash(
-      userData.password,
-      this.BCRYPT_SALT,
-    )
+    const encryptedPassword = await passwordHash(userData.password)
 
     userData.password = encryptedPassword
 
