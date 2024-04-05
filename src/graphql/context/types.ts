aac1fda383e86083b66113e24e2be405e6a4bef7
thiago nunes <desenvolvedor.nunes@gmail.com>
2024-04-04 23:11:38 -0300

    feat: adiciona forma de identificar se o token foi enviado e faz verificação

--- src/graphql/context/types.ts
+++ src/graphql/context/types.ts
@@ -3,7 +3,13 @@ import { UsersDataSourceServices } from '../schema/users/datasources'
 
 export type Context = {
   res: ServerResponse<IncomingMessage>
+  userIsLoggedIn: UserIsLoggedIn
   dataSources: {
     usersDataSource: UsersDataSourceServices
   }
+}
+
+export type UserIsLoggedIn = {
+  user_id: string
+  user_email: string
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
+import jwt from 'jsonwebtoken'
 import { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone'
 
 import { resolvers, typeDefs } from '@schema/index'
 import { UsersDataSource } from '@schema/users/datasources'
+import { UserIsLoggedIn } from '@context/types'
 
 const server = new ApolloServer({
   typeDefs,
@@ -11,8 +13,27 @@ const server = new ApolloServer({
 
 startStandaloneServer(server, {
   context: async ({ req, res }) => {
+    let tokenAndrRefreshToken: string[] = []
+
+    if (req.headers.cookie) {
+      const [refresh_token, auth_token] = req.headers.cookie.split('; ')
+      tokenAndrRefreshToken = [auth_token.split('=')[1]]
+    }
+
+    const payload = jwt.verify(
+      tokenAndrRefreshToken[0],
+      process.env.JWT_SECRET,
+      // { Desativar só quando tiver a lógica de refresh token no front-end, pq aí vai lançar um erro, e o client vai ser responsavel por criar a fila de req e atualizar o token em cada uma delas
+      //   ignoreExpiration: false,
+      // },
+    ) as UserIsLoggedIn
+
     return {
       res,
+      userIsLoggedIn: {
+        user_id: payload.user_id,
+        user_email: payload.user_email,
+      },
       dataSources: {
         usersDataSource: new UsersDataSource(),
       },
