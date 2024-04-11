1d6e899d4e222d7c95bcad7ef678cf047f2cf4e0
thiago nunes <desenvolvedor.nunes@gmail.com>
2024-04-11 17:02:16 -0300

    feat: cria typeDefs de tasks

---
+++ src/graphql/schema/tasks/typeDefs.ts
@@ -0,0 +1,59 @@
+export const tasksTypeDefs = `#graphql
+
+  extend type Mutation {
+    createTaskToProject(createTaskToProjectData: CreateTaskToProjectInput!): Task!
+  }
+
+  enum TaskStatus {
+    EM_PROGRESSO
+    FINALIZADA
+  }
+
+  enum TaskPriority {
+    Alta
+    Media
+    Baixa
+    Urgente
+  }
+
+  type GetTasksOfProjectResponse {
+    title: String!
+    description: String!
+    status: TaskStatus!
+    priority: TaskPriority!
+  }
+
+  type Task {
+    id: String!
+    title: String!
+    description: String!
+    status: TaskStatus!
+    priority: TaskPriority!
+    due_date: String!
+    comments: [Comment!]!
+    project_id: String!
+    assigned_to_id: String!
+    created_at: String!
+    updated_at: String!
+    created_by_id: String!
+  }
+
+  input CreateTaskToProjectInput {
+    title: String!
+    description: String!
+    status: TaskStatus!
+    priority: TaskPriority!
+    due_date: String!
+    project_id: String!
+    assigned_to_id: String!
+  }
+
+  type Comment {
+    id: String!
+    comment: String
+    created_at: String!
+    updated_at: String!
+    user_id: String!
+    task_id: String!
+  }
+`
---
+++ src/graphql/schema/tasks/types.ts
@@ -0,0 +1,72 @@
+export type GetTasksOfProjectResponse = {
+  title: string
+  description: string
+  status: string
+  priority: string
+}
+
+export type Task = {
+  id: string
+  title: string
+  description: string
+  status: string
+  priority: string
+  due_date: Date
+  comments: Array<Comment>
+  project_id: string
+  assigned_to_id: string
+  created_by_id: string
+  created_at: Date
+  updated_at: Date
+}
+
+export type CreateTaskToProjectInput = {
+  createTaskToProjectData: {
+    title: string
+    description: string
+    status: string
+    priority: string
+    due_date: string
+    project_id: string
+    assigned_to_id: string
+  }
+}
+
+export type Comment = {
+  id: string
+  comment?: string
+  created_at: Date
+  updated_at: Date
+  user_id: string
+  task_id: string
+}
+
+// essa aqui é pra commitar depois da createTask toda testada e commitada
+export type UpdateTaskInput = {
+  updateTaskInput: {
+    task_id: string
+    title?: string | undefined
+    description?: string | undefined
+    status?: string | undefined
+    priority?: string | undefined
+    due_date?: string | undefined
+    assigned_to_id?: string | undefined
+  }
+}
+
+// essa aqui é pra commitar depois da updateTask toda testada e commitada
+export type UpdateTaskAssignedToUserInput = {
+  updateTaskAssignedToUserInput: {
+    task_id: string
+    assigned_to_id: string
+  }
+}
+
+// essa aqui é pra commitar depois da UpdateTaskAssignedToUserInput toda testada e commitada
+export type OpenTaskFinishedInput = {
+  openTaskFinishedInput: {
+    task_id: string
+    priority: string | undefined
+    status: string
+  }
+}
