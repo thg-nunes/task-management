3225e2feab9c453ddc50eb693ba57749c2f349d6
thiago nunes <desenvolvedor.nunes@gmail.com>
2024-04-24 14:56:09 -0300

    feat: adiciona implementação para realizar carregamento em lote

--- src/graphql/dataSources/postgres.ts
+++ src/graphql/dataSources/postgres.ts
@@ -1,3 +1,4 @@
+import DataLoader from 'dataloader'
 import { Prisma, PrismaClient } from '@prisma/client'
 import { DefaultArgs } from '@prisma/client/runtime/library'
 
@@ -8,5 +9,18 @@ export class PostgresDataSource {
 
   constructor() {
     this.db = prisma
+  }
+
+  private instanceLoader = new DataLoader(async (ids) => {
+    const _ids = ids as string[]
+    return await this.batchLoaderCallback(_ids.map((id) => id))
+  })
+
+  batchLoad<T>(id: string) {
+    return this.instanceLoader.load(id)
+  }
+
+  protected async batchLoaderCallback(ids: string[]) {
+    return null
   }
 }
