5a50fda39374d937085e04f5820aec67af1dd26a
thiago nunes <desenvolvedor.nunes@gmail.com>
2024-05-03 11:43:03 -0300

    feat: adiciona config para permitir requisições cross-sitting a partir de domínios especificados

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,48 @@
+import cors from 'cors'
+import express from 'express'
 import { ApolloServer } from '@apollo/server'
-import { startStandaloneServer } from '@apollo/server/standalone'
+import { expressMiddleware } from '@apollo/server/express4'
 
 import { resolvers, typeDefs } from '@schema/index'
 import { UsersDataSource } from '@schema/users/datasources'
 import { ProjectsDataSource } from '@schema/projects/datasources'
 import { TaskDataSource } from '@schema/tasks/datasouce'
+import { Context } from '@context/types'
+;(async () => {
+  const app = express()
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-})
+  const server = new ApolloServer<Context>({
+    typeDefs,
+    resolvers,
+  })
 
-startStandaloneServer(server, {
-  context: async ({ req, res }) => {
-    return {
-      req,
-      res,
-      dataSources: {
-        taskDataSource: new TaskDataSource(),
-        usersDataSource: new UsersDataSource(),
-        projectsDataSource: new ProjectsDataSource(),
+  await server.start()
+
+  app.use(
+    '/',
+    cors({
+      credentials: true,
+      origin: [`${process.env.FRONT_END_ENDPOINT}`],
+    }),
+    express.json(),
+    expressMiddleware(server, {
+      context: async ({ req, res }) => {
+        return {
+          req,
+          res,
+          dataSources: {
+            taskDataSource: new TaskDataSource(),
+            usersDataSource: new UsersDataSource(),
+            projectsDataSource: new ProjectsDataSource(),
+          },
+        }
       },
-    }
-  },
-})
-  .then(({ url }) => console.log(`server on in url ${url}`))
-  .catch((e) => console.error(e))
+    }),
+  )
+
+  app.listen(process.env.SERVER_PORT, () =>
+    console.log(
+      `server listening: http://localhost:${process.env.SERVER_PORT}/`,
+    ),
+  )
+})()
